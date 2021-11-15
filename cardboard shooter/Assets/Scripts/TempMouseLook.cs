1b7bdb218082e0378b7fb3e4331a8e361f9016b0
Kreston101 <93973537+Kreston101@users.noreply.github.com>
2021-11-15 20:09:25 +0800

    Scene setup
    placeholders set up

---
+++ cardboard shooter/Assets/Scripts/TempMouseLook.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempMouseLook : MonoBehaviour
+{
+    public float sen = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hori = Input.GetAxis("Horizontal") * sen * Time.deltaTime;
+        float verti = Input.GetAxis("Vertical") * sen * Time.deltaTime;
+
+        transform.Rotate(verti, hori, 0);
+    }
+}
